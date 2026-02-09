55691d8d69e4f750d8d07ff4fe3ade11bf9f08a3
shelllrz <michellealejanddrina45@gmail.com>
2026-02-09 18:35:46 -0300

    add file

---
+++ logica-js-aula_2/operadores.js
@@ -0,0 +1,20 @@
+let idade = 25;
+let possuiCarteira = true;
+
+// se idade é maior que 18 e possui carteira…
+if (idade > 18 && possuiCarteira) {
+  console.log("Pode dirigir!");
+} else {
+  console.log("Não pode dirigir.");
+}
+// O operador AND, representado pelos símbolos "&&", é utilizado para combinar duas condições e avaliar se ambas são verdadeiras. Se ambas as condições forem verdadeiras, o resultado será… verdadeiro. Caso contrário, logicamente será falso. 
+// let temMaça = false;
+let temBanana = true;
+
+// se tem maça ou tem banana…
+if (temMaça || temBanana) {
+  console.log("Você tem frutas!");
+} else {
+  console.log("Não tem frutas.");
+}
+// O operador OR, representado pelos símbolos "||", é usado para verificar se pelo menos uma das condições é verdadeira. Se uma das condições for verdadeira, o resultado será verdadeiro. Se ambas forem falsas, o resultado será falso. 
